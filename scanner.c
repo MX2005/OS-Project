8ebaff599b664a05a12ea59d5a5c75d39ec15a97
MX2005 <M.Khaled2316@nu.edu.eg>
2026-05-07 00:07:11 +0300

    Scanner

---
+++ scanner.c
@@ -0,0 +1,99 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+#include "kernel/fcntl.h"
+
+
+struct file_info {
+    char path[128];
+    uint64 size;   
+    int type;      
+    uint ino;      
+};
+
+struct file_info files[1000];
+int file_count = 0;
+
+
+void pathcat(char *dst, char *src) {
+    int i = strlen(dst);
+    int j = 0;
+    while (src[j]) {
+        dst[i++] = src[j++];
+    }
+    dst[i] = '\0';
+}
+
+
+void save_metadata(char *path, uint64 size, int type, uint ino) {
+    printf("DEBUG: entering save_metadata with path length %d\n", strlen(path));
+
+    if(file_count >= 1000) {
+        printf("metadata array full\n");
+        return;
+    }
+
+    if(strlen(path) >= sizeof(files[file_count].path)) {
+        printf("path too long: %s\n", path);
+        return;
+    }
+
+    strcpy(files[file_count].path, path);
+    files[file_count].size = size;
+    files[file_count].type = type;
+    files[file_count].ino  = ino;
+
+    file_count++;
+}
+
+
+void scan_dir(char *path) {
+    int fd = open(path, O_RDONLY);
+    if(fd < 0) {
+        printf("cannot open %s\n", path);
+        return;
+    }
+
+    struct dirent de;
+    while(read(fd, &de, sizeof(de)) > 0) {
+        if(de.inum == 0) continue;
+
+        char newpath[128];
+        strcpy(newpath, path);
+        pathcat(newpath, "/");
+        pathcat(newpath, de.name);
+
+        struct stat st;
+        if(stat(newpath, &st) >= 0) {
+            if(strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0) continue;
+
+            printf("Path: %s | Size: %lu | Type: %d | Inode: %u\n",
+                   newpath,
+                   (unsigned long)st.size,
+                   st.type,
+                   st.ino);
+
+            
+            save_metadata(newpath, st.size, st.type, st.ino);
+
+            
+            if(st.type == T_DIR) {
+                scan_dir(newpath);
+            }
+        }
+    }
+    close(fd);
+}
+
+
+int main(int argc, char *argv[]) {
+    if(argc < 2) {
+        printf("Usage: scanner <directory>\n");
+        exit(0);
+    }
+
+    scan_dir(argv[1]);
+
+    exit(0);
+}
