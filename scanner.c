9500726f0171466b0d4dfe906551860a4864da5b
MX2005 <153654024+MX2005@users.noreply.github.com>
2026-05-08 20:01:17 +0300

    Update scanner.c

--- scanner.c
+++ scanner.c
@@ -9,13 +9,18 @@
 #define MAX_FILES 1000
 #define MAX_PATH 128
 
-
+struct file_info {
+    char path[MAX_PATH];
+    uint64 size;
+    int type;
+    uint ino;
+};
 
 struct file_info files[MAX_FILES];
 int file_count = 0;
 
 
-void print_metadata() {
+void print_metadata(struct file_info fi) {
     printf("\n========== SCANNED FILES ==========\n");
 
     printf("Path: %s | Size: %lu | Type: %d | Inode: %u\n",
@@ -59,7 +64,12 @@ void scan_dir(char *path) {
         if(stat(newpath, &st) >= 0) {
             if(strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0) continue;
 
-            print_metadata();
+            struct file_info fi;
+            strcpy(fi.path, newpath);
+            fi.size = st.size;
+            fi.type = st.type;
+            fi.ino = st.ino;
+            print_metadata(fi);
             
             if(st.type == T_DIR) {
                 scan_dir(newpath);
